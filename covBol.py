31e75c553aefbdc7843ba15cbfc2f72db7e060eb
gorrioncillo <pedrolc05@gmail.com>
2020-06-23 17:59:30 -0400

    Actualizando codigo y los datos a la fecha 22-06-2020

--- covBol.py
+++ covBol.py
@@ -1,19 +1,39 @@
-import urllib.request, json, pprint, os.path, csv, time, sqlite3, tinydb
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+import matplotlib
+matplotlib.use('Qt5Agg')
+import urllib.request, urllib.error,\
+  json, pprint, os.path, csv, time, sqlite3, tinydb,\
+  matplotlib.pyplot as plt, matplotlib.dates as dates
 from datetime import datetime
 
 urlBolSeg = 'https://boliviasegura.agetic.gob.bo/wp-content/json/api.php'
 bolSegCsv = 'bolSegDatos.csv'
 prevJsonData = 'oldBolSegCov.json'
-timesToCheck = 5
+timesToCheck = 1
+timeSleep = 1 #30*60 #en segundos
 bolSegSqlite = 'bolSegDatos.s3db'
 bolSegSqliteTable = 'bolCovid19'
 bolSegTinydb = 'bolSegDatos.json'
 bolSegTinydbTable = 'bolCovid19'
 
 def getDataJson():
-  datos = urllib.request.urlopen(urlBolSeg)
-  #datos = open('bolSegJson-02-05-2020.json','r')
-  jsonDatos = json.load(datos)
+  try:
+    datos = urllib.request.urlopen(urlBolSeg)
+    #datos = open('bolSegJson-02-05-2020.json','r')
+    jsonDatos = json.load(datos)
+  except urllib.error.URLError as e:
+    if hasattr(e, 'reason'):
+      print('No pudimos conectarnos al servidor')
+      print("Revize su conexion a internet")
+      print('Razon: ', e.reason)
+      return None
+    elif hasattr(e, 'code'):
+      print('El servidor no pudo manejar el pedido')
+      print('Error code: ', e.code)
+      return -1
+  #except de json read
   return jsonDatos
 
 def checkOldDatetimeJsonFile(newJsonData):
@@ -30,7 +50,8 @@ def checkOldDatetimeJsonFile(newJsonData):
 
 def newToOldJson(newjsonData):
   with open(prevJsonData,'w', encoding= 'utf-8') as oldFile:
-    json.dump(newjsonData, oldFile, ensure_ascii=False)
+    json.dump({'fecha':newjsonData['fecha']}, oldFile, ensure_ascii=False)
+    #json.dump(newjsonData, oldFile, ensure_ascii=False)
 
 def flattenJson(objJson, delim):
   val = {}
@@ -65,15 +86,29 @@ def putDataSqlite(database, objDic, table='bolCovid19'):
   conn.close()
 
 def getDataSqlite(database, table='bolCovid19'):
+  bolDatetime = []
+  bolCases = []
+  bolDeceases = []
+
+  cbbaCases = []
+  cbbaDeceases = []
+
   query = f'SELECT * FROM {table}' #bolCovid19
   conn = sqlite3.connect(database)
   c = conn.cursor()
   c.execute(query)
   rows = c.fetchall()
+
   for row in rows:
     m = json.loads(row[0])
-    print(m['fecha'], m['contador']['confirmados'], m['contador']['decesos'])
+    bolDatetime.append(datetime.strptime(m['fecha'].split(' ')[0], '%d/%m/%y'))
+    bolCases.append(m['contador']['confirmados'])
+    bolDeceases.append(m['contador']['decesos'])
+    cbbaCases.append(m['departamento']['cb']['contador']['confirmados'])
+    cbbaDeceases.append(m['departamento']['cb']['contador']['decesos'])
+  
   conn.close()
+  return bolDatetime, bolCases, bolDeceases, cbbaCases, cbbaDeceases
 
 def putDataTinydb(database, objDic, table='bolCovid19'):
   db = tinydb.TinyDB(database)
@@ -89,36 +124,119 @@ def getDataTinydb(database, table='bolCovid19'):
     print(r['fecha'], r['contador']['confirmados'], r['contador']['decesos'])
   db.close()
 
+def newPeople(tupData):
+  #q = tupData[0]
+  a = [(tupData[1][n]-tupData[1][n-1]) for n in range(1,len(tupData[1]))]
+  a.insert(0,0)
+  b = [(tupData[2][n]-tupData[2][n-1]) for n in range(1,len(tupData[2]))]
+  b.insert(0,0)
+  c = [(tupData[3][n]-tupData[3][n-1]) for n in range(1,len(tupData[3]))]
+  c.insert(0,0)
+  d = [(tupData[4][n]-tupData[4][n-1]) for n in range(1,len(tupData[4]))]
+  d.insert(0,0)
+  return a,b,c,d
 
-def main(): 
+def ploting(n, x, y ,z, w, v):
+  x = dates.date2num(x)
+  hfmt = dates.DateFormatter('%m\n%d')
+
+  fig = plt.figure(n)
+  axs = fig.subplots(nrows=2, ncols=1)
+
+  axs[0].bar(x, w, width=0.15, alpha=0.5, color='yellow',
+            label='Nuevos Casos')
+  axs01 = axs[0].twinx()
+  axs01.scatter(x, y, color='blue', label='Casos Acumulados')
+  #ax = fig.gca()
+  axs[0].xaxis.set_major_locator(dates.DayLocator())
+  axs[0].xaxis.set_major_formatter(hfmt)
+
+
+  axs[1].bar(x, v, width=0.15, alpha=0.5, color='black',
+            label='Nuevas Muertes')
+  axs11 = axs[1].twinx()
+  axs11.scatter(x, z, color='red', label='Muertes Acumuladas')
+
+  axs[1].xaxis.set_major_locator(dates.DayLocator())
+  axs[1].xaxis.set_major_formatter(hfmt)
+  
+  #Formateo la caja de informacion de coordenadas
+  axs01.format_xdata = dates.DateFormatter('%d-%m-%Y')
+  axs01.format_ydata = lambda x: '%1.0f Casos' % x
+  axs01.grid(True)
+  axs11.format_xdata = dates.DateFormatter('%d-%m-%Y')
+  axs11.format_ydata = lambda x: '%1.0f Casos' % x
+  axs11.grid(True)
+  fig.autofmt_xdate()
+  fig.subplots_adjust(top=0.88, bottom=0.1, left=0.05, right=0.94,
+                      hspace=0.1, wspace=0.2)
+  fig.legend(loc='upper center', ncol=4, bbox_to_anchor=(0.5,0.955))
+  return fig
+
+def printTable(hed, *args):
+  formatStr = "{:<11}" + ("{:<9}" * (len(hed)-1))
+  print(formatStr.format(*hed))
+  for i,v in enumerate(args[0]):
+    rec = [it[i] for it in args]
+    print(formatStr.format(*rec))
+
+
+def main():
   pass
-
+  
 if __name__ == '__main__':
+  print("Comenzando el programa\n")
   #main()
   do = True
   counter = 0
   while(do and (counter<timesToCheck)):
     jsonData = getDataJson()
-    #pprint.pprint(jsonDatos)
+    if not isinstance(jsonData, dict):
+      break
+    elif isinstance(jsonData, int):
+      counter +=1
+      print("Intentando nuevamente")
+      continue
+    
     isNewJson = checkOldDatetimeJsonFile(jsonData)
-    print(isNewJson)
+
     if(isNewJson):
       newToOldJson(jsonData)
       flatDic = flattenJson(jsonData,'.')
-      #pprint.pprint(flatDic)
       columnas = putDataCsv(bolSegCsv, flatDic)
-      #pprint.pprint(columnas)
       putDataSqlite(bolSegSqlite, jsonData, bolSegSqliteTable)
       putDataTinydb(bolSegTinydb, jsonData, bolSegTinydbTable)
       do = False
     else:
-      time.sleep(1)#30*60)
+      print(f"No hay nuevos datos.\nIntentando en {timeSleep} segs")
       counter += 1
+      time.sleep(timeSleep)
   
   if (counter==timesToCheck):
-    print(f'Se intento {timesToCheck} veces')
-    print('No se encontraron nuevos datos')
+    print(f'\nSe intento {timesToCheck} veces')
+    print('No se encontraron nuevos datos\n')
 
-  getDataSqlite(bolSegSqlite, bolSegSqliteTable)
+  aux1 = getDataSqlite(bolSegSqlite, bolSegSqliteTable)
+  xDate, aCases, aDeaths, cbCases, cbDeaths = aux1
+  xDateStr = [x.date().strftime('%d-%m-%y') for x in xDate]
+  aux2 = newPeople(aux1)
+  aNewCases, aNewDeaths,cbNewCases,cbNewDeaths = aux2
+
+  print('{:<46} {:<46}'.format('','Nuevos'))
+  headers=['Fecha', 'Casos', 'Muertes','cbCasos', 'cbMuet',
+          'Casos', 'Muertes', 'cbCasos', 'cbMuet']
+  aux2 = aux1 + aux2
+  if(len(xDateStr)>=20):
+    printTable(headers, xDateStr[-20:], *[a[-20:] for a in aux2[1:]])
+  else:
+    printTable(headers, xDateStr, *aux2[1:])
+  
+  #fig1 = ploting(1, xDate, aCases, aDeaths, aNewCases, aNewDeaths)
+  #fig1.suptitle('Grafica Bolivia')
+  #fig1.canvas.set_window_title('Graficas Bolivia')
+  #fig2 = ploting(2, xDate, cbCases, cbDeaths, cbNewCases, cbNewDeaths)
+  #fig2.suptitle('Grafica Cochabamba')
+  #fig2.canvas.set_window_title('Graficas Cochabamba')
+  #plt.show()
   print()
-  getDataTinydb(bolSegTinydb, bolSegTinydbTable)
+  #getDataTinydb(bolSegTinydb, bolSegTinydbTable)
